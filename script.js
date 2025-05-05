ae6ee550e05fbca871ab76fec6e28246327ab5cc
Vihnnn <yrvihnbargola24@gmail.com>
2025-05-05 15:51:15 +0800

    add Dom in js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Put your solution here.
+const form = document.getElementById("form");
+const outputDiv = document.getElementById ("output");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const userInput = form.querySelectorAll("input");
+    var output = "";
+    userInput.forEach(function (input) {
+        console.log(input.value);
+        output += input.placeholder +": "+input.value + "<br>";
+        console.log("hello World")
+    });
+    outputDiv.innerHTML = output;
+})
